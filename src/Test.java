e4ea8a53089b2d731c5c013bf2ca86c857f9f73b
Juliana501 <julianakseny@gmail.com>
2019-08-31 23:33:49 +0300

    Задачка 4. Ввести с консоли число в бинарном формате. Перевести его в десятичный формат, записать в переменную int и вывести на экран.

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Test {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input = reader.readLine();
+        try {
+            int a = Integer.parseInt(input);
+            int result = 0;
+            int exponent = 0;
+            int currentSymbol;
+            while (a > 0) {
+                currentSymbol = a % 10;
+                if ((currentSymbol != 0 ) & (currentSymbol != 1)) {
+                    throw new NumberFormatException();
+                 }
+                result += currentSymbol * Math.pow(2, exponent);
+                exponent++;
+                a = a / 10;
+            }
+            System.out.println(result);
+        } catch (NumberFormatException e) {
+            System.out.println("Введи число в бинарном формате");
+        }
     }
 
 }
